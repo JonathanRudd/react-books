c09ea28f09a02bf517890658c5e8fa643bd1f0f6
Jonathan Rudd <jonathanrudd3@gmail.com>
2023-06-27 20:34:04 +0900

    delete, edit and create REST functions added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from 'axios';
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
 
 function App() {
   const [books, setBooks] = useState([]);
+
+  const fetchBooks = async () => {
+    const response = await axios.get('http://localhost:3001/books');
+    setBooks(response.data);
+  };
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
 
   const createBook = async (title) => {
     const response = await axios.post('http://localhost:3001/books', {
@@ -18,12 +28,17 @@ function App() {
     setBooks(updatedBooks);
   };
 
-  const editBookById = (id, newTitle) => {
+  const editBookById = async (id, newTitle) => {
+    const response = await axios.patch(`http://localhost:3001/books/${id}`, {
+      title: newTitle,
+    });
+
+
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
         return {
           ...book,
-          title: newTitle,
+          ...response.data
         };
       } else {
         return book;
@@ -32,7 +47,9 @@ function App() {
     setBooks(updatedBooks);
   };
 
-  const deleteBookById = (id) => {
+  const deleteBookById = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
+
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
     });
